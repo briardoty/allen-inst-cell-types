da184fcb93fe6524b795667eecf74abe4e89f940
Briar Doty <briardoty@gmail.com>
2020-07-16 09:25:33 -0700

    Update refs to processor

--- modules/Visualizer.py
+++ modules/Visualizer.py
@@ -5,12 +5,10 @@ Created on Tue Jul  7 14:22:11 2020
 
 @author: briardoty
 """
-import torch.nn as nn
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.backends.backend_pdf
-from .NetManager import get_net_tag, NetManager
+from .StatsProcessor import StatsProcessor
 
 
 
@@ -21,7 +19,7 @@ class Visualizer():
         self.data_dir = data_dir
         self.save_fig = save_fig
         
-        self.net_manager = NetManager(net_name, n_classes, data_dir)
+        self.stats_processor = StatsProcessor(net_name, n_classes, data_dir)
         
     def plot_accuracy(self, case_ids):
         """
@@ -35,7 +33,7 @@ class Visualizer():
 
         """
         # pull data
-        acc_df = self.net_manager.load_accuracy_df(case_ids)
+        acc_df = self.stats_processor.load_accuracy_df(case_ids)
 
         # group and compute stats        
         acc_df.set_index(["case", "epoch"], inplace=True)
@@ -64,78 +62,28 @@ class Visualizer():
             plt.show()
             return
         
-        sub_dir = self.sub_dir(f"figures/{self.net_manager.net_name}/")
+        sub_dir = self.sub_dir(f"figures/{self.stats_processor.net_name}/")
         cases = " & ".join(case_ids)
         filename = f"{cases} accuracy.png"
         filename = os.path.join(sub_dir, filename)
         print(f"Saving... {filename}")
         plt.savefig(filename, dpi=100)  
-                    
         
-    def plot_filters(self, i_layer, case_id, sample, epoch):
+    def plot_weight_changes(self, case_ids):
         """
-        Plots visualizations of filters
+        Plots average change in weights over training for the given
+        experimental cases.
 
         Args:
-            layer_name (str): Name of layer.
-            epochs (list): Epochs to plot filters over.
+            cases (list): Experimental cases to include in figure.
 
         Returns:
             None.
 
         """
-        # TODO: loop over multiple epochs?
         
-        # load net
-        net = self.net_manager.load_net_snapshot(case_id, sample, epoch)
         
-        # extract kernels
-        layer = net.features[i_layer]
-        kernels = layer.weight.data
-        if not isinstance(layer, nn.Conv2d):
-            print()
-            return
-        n_kernels = kernels.shape[0]
-        
-        # define sub plots
-        n_cols = 20
-        n_rows = n_kernels
-        
-        # create fig
-        fig = plt.figure(figsize=(n_cols, n_rows))
-        
-        # iterate over kernels
-        for i in range(n_kernels):
-            ax = fig.add_subplot(n_rows, n_cols, i+1)
-            
-            # denormalize and convert
-            npimg = np.array(kernels[i].numpy(), np.float32)
-            npimg = (npimg - np.mean(npimg)) / np.std(npimg)
-            npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
-            npimg = npimg.transpose((1, 2, 0))
-            
-            # plot
-            ax.imshow(npimg)
-            ax.axis('off')
-            ax.set_title(str(i))
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-        
-        plt.tight_layout()
-        plt.show()
-
-        # below here it's all about saving
-        if not self.save_fig:
-            print("Not saving.")
-            return
-        
-        sub_dir = self.sub_dir(f"figures/{self.net_manager.net_name}/{self.net_manager.case_id}/sample-{self.net_manager.sample}/")
-        net_tag = get_net_tag(self.net_name, self.case_id, self.sample, 
-                              self.net_manager.epoch)
-        filename = f"{net_tag}.png"
-        filename = os.path.join(sub_dir, filename)
-        print(f"Saving... {filename}")
-        plt.savefig(filename, dpi=100)    
+        return   
         
     def sub_dir(self, sub_dir):
         """
@@ -156,11 +104,5 @@ class Visualizer():
             
         return sub_dir
         
-        
-if __name__=="__main__":
-    # init visualizer
-    visualizer = Visualizer(data_dir, net_name, n_classes, True)
-    
-    # plot
-    visualizer.plot_accuracy(case_ids)
+
 
