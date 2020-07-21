ff291e5d51a6e6ecc07ee216071c0a10e86731a6
Briar Doty <briardoty@gmail.com>
2020-07-21 16:09:04 -0700

    Simplify net gen case names

--- gen_nets.py
+++ gen_nets.py
@@ -19,7 +19,7 @@ parser.add_argument("--n_samples", default=10, type=int, help="Set value for n_s
 # config params without defaults
 parser.add_argument("--case", type=str, help="Set value for case")
 parser.add_argument("--layer_names", type=str, nargs="+", help="Set value for layer_names")
-parser.add_argument("--n_repeat_arr", type=int, nargs="+", help="Set value for n_repeat_arr")
+parser.add_argument("--n_repeat", type=int, help="Set value for n_repeat")
 parser.add_argument("--act_fns", type=str, nargs="+", help="Set value for act_fns")
 parser.add_argument("--act_fn_params", type=str, nargs="+", help="Set value for act_fn_params")
 
@@ -29,50 +29,27 @@ pretrained_parser.add_argument('--pretrained', dest='pretrained', action='store_
 pretrained_parser.add_argument('--untrained', dest='pretrained', action='store_false')
 parser.set_defaults(pretrained=False)
 
-def main(case, layer_names, n_repeat_arr, act_fns, act_fn_params, data_dir, 
+def main(case, layer_names, n_repeat, act_fns, act_fn_params, data_dir, 
          net_name, n_classes, n_samples, pretrained):
     
     # init net manager
     manager = NetManager(net_name, n_classes, data_dir, pretrained=pretrained)
     
     # build and save nets
-    if "control" in case:
+    for i in range(n_samples):
+
+        # init net
+        manager.init_net(case, i)
+        
         # control nets are unmodified
-        for i in range(n_samples):
-            gen_control_net(manager, case, i)
-    
-    else:
-        # mixed nets require modification
-        for n_repeat in n_repeat_arr:
-            
-            layers = "-".join(layer_names)
-            
-            # each layer set/n_repeat deserves its own case id
-            case_id = f"{case}_{layers}_nr-{n_repeat}"
-            
-            for i in range(n_samples):
-                gen_net(manager, case_id, i, layer_names, n_repeat, act_fns, 
-                        act_fn_params)
-            
+        if not "control" in case:
+            manager.replace_layers(layer_names, n_repeat, act_fns, act_fn_params)
+        
+        # save
+        manager.save_net_snapshot()
+
     print(f"gen_nets.py completed case {case}")
-    return
-
-def gen_control_net(manager, case, i):
-    # init net
-    manager.init_net(case, i)
-    
-    # save
-    manager.save_net_snapshot()
-
-def gen_net(manager, case_id, i, layer_names, n_repeat, act_fns, act_fn_params):
-    # init net
-    manager.init_net(case_id, i)
-    
-    # modify
-    manager.replace_layers(layer_names, n_repeat, act_fns, act_fn_params)
-    
-    # save
-    manager.save_net_snapshot()
+    return   
 
 if __name__=="__main__":
     args = parser.parse_args()
